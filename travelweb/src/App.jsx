60ee731531845f7da2d7180f02a1702cef3302d3
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-02-12 10:03:17 +0530

    c

--- travelweb/src/App.jsx
+++ travelweb/src/App.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Features from "./components/Features";
+import Destination from "./components/Destination";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 function App() {
-
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <main>
+        <div id="home">
+          <Home />
+        </div>
+
+        <div id="features">
+          <Features />
+        </div>
+
+        <div id="destination">
+          <Destination />
+        </div>
+
+        <div id="about">
+          <About />
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
+
+        <div id="footer">
+          <Footer />
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
